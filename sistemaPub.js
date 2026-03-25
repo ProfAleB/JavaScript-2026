5cc37081e648c3eab49eb9525656c5869399cee2
Alexandre Boeing <140010324+ProfAleB@users.noreply.github.com>
2026-03-25 13:02:24 +0000

    aula 5

---
+++ sistemaPub.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync');
+
+console.log("Sistema de acesso ao Pub do John");
+
+const nome = readline.question("Qual seu nome? ");
+const idade = readline.questionInt("Qual sua idade? ");
+
+if (idade >= 18) {
+    console.log(`Olá ${nome}, sua entrada foi Autorizada. `);
+    const querBeber = readline.keyInYN("Deseja ver as bebidas? ");
+    if (querBeber){
+        console.log("Garçom a caminho! ");
+    } else{
+        console.log("Temos sucos também");
+    }
+} else if (idade >= 16){
+    console.log(`Olá ${nome}, você precisa estar acompanhado.. `);
+    const jovem = readline.keyInYN("Deseja refri? ");
+    if (jovem){
+        console.log("Temos pepsi e coca! ");
+    } else{
+        console.log("Temos suco e água com gás");
+    }
+} else{
+    console.log(`Sinto muito ${nome}, apenas maiores que 16....`)
+}
